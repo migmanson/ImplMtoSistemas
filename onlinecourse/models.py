4f22f1e3f304febb66af5de23e9af3a192635190
Jeff Hamblin <jeff_hamblin@hotmail.com>
2022-03-22 06:41:33 +0000

    Fixed bug where incorrect answers were still accepted.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -110,7 +110,8 @@ class Question(models.Model):
     def is_get_score(self, selected_ids):
        all_answers = self.choice_set.filter(is_correct=True).count()
        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-       if all_answers == selected_correct:
+       selected_incorrect = self.choice_set.filter(is_correct=False, id__in=selected_ids).count()
+       if all_answers == selected_correct and selected_incorrect == 0:
            return True
        else:
            return False
